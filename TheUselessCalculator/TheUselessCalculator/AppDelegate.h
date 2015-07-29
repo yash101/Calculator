86b829f00f3e1cf1716e6824d5ff5a6881bf190d
Dev <dlodha@pvlearners.net>
2015-07-29 14:49:38 -0700

    Created the Xcode project. Still reading an iOS book, so haven't done any code.
    Committing just to save.

---
+++ TheUselessCalculator/TheUselessCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheUselessCalculator
+//
+//  Created by Devyash Lodha on 7/28/15.
+//  Copyright (c) 2015 Devyash Lodha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheUselessCalculator/TheUselessCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheUselessCalculator
+//
+//  Created by Devyash Lodha on 7/28/15.
+//  Copyright (c) 2015 Devyash Lodha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
